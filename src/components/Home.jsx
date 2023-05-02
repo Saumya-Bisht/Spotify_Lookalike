71b7fa67af7e98f3db00cee7380bef6f98b12ee1
Saumya-Bisht <saumyabisht05@gmail.com>
2023-05-03 01:26:03 +0530

    spotify

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,22 +3,22 @@ import {Link} from 'react-router-dom'
 
 function Home() {
     let[vid,setVid]=useState({})
-  //   useEffect(()=>{
-  //       fetch("https://spotify23.p.rapidapi.com/search/?q=${q}&type=multi",{
-  //           method:"GET",
-  // params: {
-  //   q: '<REQUIRED>',
-  //   type: 'multi',
-  //   offset: '0',
-  //   limit: '10',
-  //   numberOfTopResults: '5'
-  // },
-  // headers: {
-  //   'X-RapidAPI-Key': 'a7cd13a03bmsh2a04d106eaead2dp1c6c2fjsn4a7170c7091d',
-  //   'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-  // }
-  //       }).then((res)=>res.json()).then((data)=>{setVid(data);console.log(data)})
-  //   },[])
+    useEffect(()=>{
+        fetch("https://spotify23.p.rapidapi.com/search/?q=${q}&type=multi",{
+            method:"GET",
+  params: {
+    q: '<REQUIRED>',
+    type: 'multi',
+    offset: '0',
+    limit: '10',
+    numberOfTopResults: '5'
+  },
+  headers: {
+    'X-RapidAPI-Key': 'a7cd13a03bmsh2a04d106eaead2dp1c6c2fjsn4a7170c7091d',
+    'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+  }
+        }).then((res)=>res.json()).then((data)=>{setVid(data);console.log(data)})
+    },[])
 let linksty={
   color:"white",
   textDecoration:"none",
@@ -34,7 +34,7 @@ let psty={
     <div style={{backgroundColor:"#212121"}}>
         <h2>Popular Playlists</h2>
         {console.log(vid)}
-         {/* <div style={{display:"flex",flexWrap:"wrap"}}>{vid.playlists.items?.map((ele)=>{
+         <div style={{display:"flex",flexWrap:"wrap"}}>{vid.playlists.items?.map((ele)=>{
             return (<div style={{boxShadow:"0px 0px 10px 3px #121212",borderRadius:"10px",padding:"10px",margin:"10px",width:"13vw",display:"flex",flexDirection:"column"}}>
             <Link style={linksty} to="/details"><img  width="100%" src={ele.data.images.items[0].sources[0].url} alt="img"/>
                <p> {ele.data.name} </p>
@@ -67,7 +67,7 @@ let psty={
                
                
                )
-       })}</div>  */}
+       })}</div> 
      
     </div>
   )
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,21 +2,42 @@ import React ,{useState,useEffect}from 'react'
 
 
 function Search() {
-  // const [searchQuery, setSearchQuery] = useState('');
+  const [inp, setInp] = useState('');
   const[button,setButton]=useState([]);
+  const[vid,setVid]=useState([]);
+
+
+  useEffect(()=>{
+          fetch("https://spotify23.p.rapidapi.com/search/?q=~${inp}",{
+              method:"GET",
+    params: {
+      q: '<REQUIRED>',
+      type: 'multi',
+      offset: '0',
+      limit: '10',
+      numberOfTopResults: '5'
+    },
+    headers: {
+      'X-RapidAPI-Key': 'a7cd13a03bmsh2a04d106eaead2dp1c6c2fjsn4a7170c7091d',
+      'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    }
+          }).then((res)=>res.json()).then((data)=>{setVid(data);console.log(data)})
+      },[])
 
   const show=()=>{
-    return setButton(<div>
-        <h1>hiii</h1>
-    </div>)
+     setButton(inp)
+     console.log(vid)
   }
   const clear=()=>{
-    return setButton([])
+   setButton([])
+   setInp("")
+
   }
+  // console.log(inp)
   return (
     <div  style={{}}>
         <h1>Search component</h1>
-        <input style={{width:"30vw",height:"30px",borderRadius:"20px",border:"0"}} type='text' placeholder=' 🔍 What do you want to listen to!?'/>
+        <input value={inp} onChange={(e)=>setInp(e.target.value)} style={{width:"30vw",height:"30px",borderRadius:"20px",border:"0"}} type='text' placeholder=' 🔍 What do you want to listen to!?'/>
         <button style={{backgroundColor:"white",border:"1px solid white",marginLeft:"5px",fontWeight:"bold",cursor:"pointer"}} onClick={show}>Search</button>
         <button style={{backgroundColor:"white",border:"1px solid white",marginLeft:"5px",fontWeight:"bold",cursor:"pointer"}} onClick={clear}>Clear</button>
         <div>
